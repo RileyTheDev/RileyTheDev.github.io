bbefda158db4bc9826f66db501d0f89e4091dc70
RileyTheDev <51127072+RileyTheDev@users.noreply.github.com>
2022-02-14 12:39:47 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+<html><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <title>RileyTheDev</title>
+    
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Inconsolata:400,700%27);
+    </style>
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <div class="header">
+    <center>
+        <element style="font-family: Inconsolata; font-size: 14px;">
+        <span style="font-size: 30px;">RileyTheDev‎‎‎‎‎                </span> <span style="font-size: 20px;"><a href="/me">about</a> <a href="https://github.com/RileyTheDev%22%3Egithub</a></span>
+
+
+
+</div></center></element>
+
+
+</div></center></element>
+
+<div class="blog">
+    <center><element style="font-family: Inconsolata; font-size: 14px;">
+
+    <h1>Blog Posts:</h1>
+    <ul>
+
+        <li>
+            <span class="date">Feburary 14 2022</span> - <a href="/blog/web-app-in-2016">Switching from Windows To Linux, A beginners perspective</a>
+        </li>
+
+    </ul>
+</div></center></element>
+
+<div class="Projects">
+    <center><element style="font-family: Inconsolata; font-size: 14px;">
+
+    <h1>Projects:</h1>
+    <ul>
+
+        <li>
+            <span class="date">August 21, 2021</span> - <a href="https://github.com/RileyTheDev/Panda-Bot%22%3EPanda Bot</a>
+        </li>
+
+    </ul>
+</div></center></element></element>
+
+
+
+</body></html>
